7717f786ef3635905d2c52e5f6333857a07fa5f9
pradeepsha <pradeepkumar29786@gmail.com>
2024-02-02 22:15:28 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# javascript
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/pradeepsha/javascript)
---
+++ index.js
@@ -0,0 +1,4 @@
+// run `node index.js` in the terminal
+// let promises = require("./");
+
+console.log(`Hello Node.js v${process.versions.node}!`);
